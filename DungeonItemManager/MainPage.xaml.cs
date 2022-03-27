d1ce82ec640dac4b06e752224f8a548327592271
Crafterchen2 <74384421+Crafterchen2@users.noreply.github.com>
2022-03-27 19:20:08 +0200

    Projektdateien hinzufügen.

---
+++ DungeonItemManager/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace DungeonItemManager
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void nvSample_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            Type pageType = typeof(ItemsPage);
+            String Tag = (String) ((Microsoft.UI.Xaml.Controls.NavigationViewItem)nvSample.SelectedItem).Tag;
+            switch (Tag)
+            {
+                case "Home":
+                    pageType = typeof(HomePage);
+                    break;
+            }
+            contentFrame.NavigateToType(pageType, null, null);
+
+        }
+    }
+}
